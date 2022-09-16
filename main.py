75d71b0951837303c3aafc90fc758b6451f1350b
25prajwal <94011967+25prajwal@users.noreply.github.com>
2022-09-16 17:31:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/sum/<string:n>/<string:m>')
+def sum(n,m):
+    a = int(n)+int(m)
+    return str(a)
+
+@app.route('/web/<string:name>')
+def web(name):
+    from websearch import WebSearch
+    web = WebSearch(name)
+    return web.pages
+
+@app.route('/web/<string:name>/<string:ext>')
+def web_(name,ext):
+    from websearch import WebSearch
+    web = WebSearch(f"{name} filetype:{ext}")
+    return web.pages
+
+
+if __name__ == "__main__" :
+    app.run(debug=True)
