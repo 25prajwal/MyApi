7920a0cb135e58ddbdc275a66edd38cb2b7365e7
25prajwal <94011967+25prajwal@users.noreply.github.com>
2023-03-28 18:12:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ app = Flask(__name__)
 def home():
     return ("Hello World v1")
 
-@app.route("/xpshort/<path:url>")
-def xpshort(url):
-    import time
-    import requests
-    from bs4 import BeautifulSoup 
-    client = requests.session()
-    DOMAIN = "https://xpshort.com/"
-    url = url[:-1] if url[-1] == '/' else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"
-    ref = "https://www.jankarihoga.com/"
-    h = {"referer": ref}
-    resp = client.get(final_url,headers=h)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    inputs = soup.find_all("input")
-    data = { input.get('name'): input.get('value') for input in inputs }
-    h = { "x-requested-with": "XMLHttpRequest" }  
-    time.sleep(8)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()['url']
-    except: 
-        return "Something went wrong :("
+# @app.route("/xpshort/<path:url>")
+# def xpshort(url):
+#     import time
+#     import requests
+#     from bs4 import BeautifulSoup 
+#     client = requests.session()
+#     DOMAIN = "https://xpshort.com/"
+#     url = url[:-1] if url[-1] == '/' else url
+#     code = url.split("/")[-1]
+#     final_url = f"{DOMAIN}/{code}"
+#     ref = "https://www.jankarihoga.com/"
+#     h = {"referer": ref}
+#     resp = client.get(final_url,headers=h)
+#     soup = BeautifulSoup(resp.content, "html.parser")
+#     inputs = soup.find_all("input")
+#     data = { input.get('name'): input.get('value') for input in inputs }
+#     h = { "x-requested-with": "XMLHttpRequest" }  
+#     time.sleep(8)
+#     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+#     try:
+#         return r.json()['url']
+#     except: 
+#         return "Something went wrong :("
     
     
 if __name__ == "__main__":
